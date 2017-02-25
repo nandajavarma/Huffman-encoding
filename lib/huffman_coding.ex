aa8274a8ef2958b6121a5803a2aedd1069ef80ac
Nandaja Varma <nandaja.varma@gmail.com>
2017-02-25 00:53:44 -0500

    Adding the huffman tree creator code

--- lib/huffman_coding.ex
+++ lib/huffman_coding.ex
@@ -16,7 +16,51 @@ defmodule HuffmanCoding do
   end
 
 
-  defp balance_heap(heap, new_item, index) do
+  defp balance(heap, index) do
+    parent = elem(heap, index)
+    try do
+      lchild = elem(heap, 2*index)
+      rchild = elem(heap, 2*index + 1)
+      small_pos =
+        case elem(lchild, 1) > elem(rchild, 1) do
+          true -> 2*index + 1
+          _ ->  2*index
+        end
+      case elem(lchild, 1) <= elem(parent, 1) or elem(rchild, 1) <= elem(parent, 1) do
+        true -> put_elem(heap, index, elem(heap, small_pos)) |>
+                  put_elem(small_pos, parent) |>
+                    balance(small_pos)
+        false -> heap
+      end
+    rescue
+      _ -> heap
+    end
+  end
+
+  def remove_from_heap({}), do: {{}, nil}
+
+  def remove_from_heap(heap) do
+    tsize = tuple_size(heap)
+    {min, new_heap} =
+          case tsize do
+            1 -> {elem(heap, 0), {}}
+            2 -> first = elem(heap, 0)
+                 second = elem(heap, 1)
+                 case elem(first, 1) > elem(second, 1) do
+                   true -> {second, Tuple.delete_at(heap, 1)}
+                   _ -> {first, Tuple.delete_at(heap, 0)}
+                 end
+            _ ->
+                  rep = elem(heap, tsize - 1)
+                  {elem(heap, 1),
+                      Tuple.delete_at(heap, tsize - 1) |>
+                            put_elem(1, rep) |>
+                              balance(1)}
+          end
+    {new_heap, min}
+  end
+
+  defp insert_into_heap(heap, new_item, index) do
     parent_index = round Float.floor(index/2)
     try do
       parent = elem(heap, parent_index)
@@ -26,7 +70,7 @@ defmodule HuffmanCoding do
                   Tuple.insert_at(parent_index, new_item) |>
                     Tuple.delete_at(index) |>
                       Tuple.insert_at(index, parent) |>
-                        balance_heap(new_item, parent_index)
+                        insert_into_heap(new_item, parent_index)
         false -> heap
       end
     rescue
@@ -36,14 +80,38 @@ defmodule HuffmanCoding do
 
   def heapify(freqlist, heap \\ {}, index \\ 0)
 
-  def heapify([], heap , _index), do: heap
+  def heapify([], heap , _index), do: Tuple.insert_at(heap, 0, {0, 0})
 
   def heapify([head | tail], heap , index) do
     case heap == {} do
       true -> heapify(tail, {head}, index + 1)
-      _    -> heap = balance_heap(Tuple.append(heap, head), head, index)
+      _    -> heap = insert_into_heap(Tuple.append(heap, head), head, index)
               heapify(tail, heap, index + 1)
     end
-
   end
+
+  def create_huffman_tree(hmap, {}), do: hmap
+
+  def create_huffman_tree(hmap, heap) do
+    {new_heap, min} = remove_from_heap(heap)
+    if new_heap == {} do
+      new_val = hmap['val'] + elem(min, 1)
+      create_huffman_tree(%{'val' => new_val, 'right' => hmap, 'left' => min}, new_heap)
+    else
+      create_huffman_tree(hmap, new_heap)
+    end
+  end
+
+  def create_huffman_tree(heap) do
+    {new_leaf, heap} = create_leaf_node(heap)
+    create_huffman_tree(new_leaf, heap)
+  end
+
+  def create_leaf_node(heap) do
+    {new_heap, min1} = remove_from_heap(heap)
+    {new_heap, min2} = remove_from_heap(new_heap)
+    new_val = elem(min1, 1) + elem(min2, 1)
+    {%{'val' => new_val, 'left' => min1, 'right' => min2}, Tuple.delete_at(new_heap, 0)}
+  end
+
 end
--- test/huffman_coding_test.exs
+++ test/huffman_coding_test.exs
@@ -4,13 +4,30 @@ defmodule HuffmanCodingTest do
 
   @string "hip hip hurray"
   @frequency_list [{"H", 3}, {"I", 2}, {"P", 2}, {"R", 2}, {"A", 1}, {"U", 1}, {"Y", 1}]
+  @minheap {{0, 0}, {"A", 1}, {"U", 1}, {"I", 2}, {"Y", 1}, {"H", 3}, {"P", 2}, {"R", 2}}
   test "calculate the frequency of each character in the string" do
     assert HuffmanCoding.frequency("") == %{}
     assert HuffmanCoding.frequency(@string) == @frequency_list
   end
 
   test "heapify the frequency matrix" do
-    assert HuffmanCoding.heapify([]) == {}
-    assert HuffmanCoding.heapify(@frequency_list) == {{"A", 1}, {"U", 1}, {"I", 2}, {"Y", 1}, {"H", 3}, {"P", 2}, {"R", 2}}
+    assert HuffmanCoding.heapify([]) == {{0,0}}
+    assert HuffmanCoding.heapify(@frequency_list) == {{0, 0},{"A", 1}, {"U", 1}, {"I", 2}, {"Y", 1}, {"H", 3}, {"P", 2}, {"R", 2}}
+  end
+
+  test "get the minimum element from a minheap" do
+    assert HuffmanCoding.remove_from_heap({}) == {{}, nil}
+
+    assert HuffmanCoding.remove_from_heap(@minheap) == {{{0, 0}, {"U", 1}, {"Y", 1}, {"I", 2}, {"R", 2}, {"H", 3}, {"P", 2}}, {"A", 1}}
+  end
+
+  test "create the first element for huffman tree" do
+    assert HuffmanCoding.create_leaf_node(@minheap) == {%{'left' => {"A", 1}, 'right' => {"U", 1}, 'val' => 2},
+                                                      {{"Y", 1}, {"R", 2}, {"I", 2}, {"P", 2}, {"H", 3}}}
+  end
+
+  test "create the huffman tree" do
+    assert HuffmanCoding.create_huffman_tree(@minheap) == %{'left' => {"H", 3}, 'right' => %{'left' => {"A", 1}, 'right' => {"U", 1}, 'val' => 2},
+                                  'val' => 5}
   end
 end
